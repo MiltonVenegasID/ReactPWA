af9b5baab3b37b8be145ca9738ce456c9f4fea12
MiltonVenegas <miltonprofesional15@gmail.com>
2024-11-19 19:55:54 -0600

    LastChance

--- serviceworker.js
+++ serviceworker.js
@@ -1,21 +1,24 @@
-var staticCacheName = "pwa5";
- 
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]).catch(function (error) {
-        console.error("Failed to cache resources:", error);
-      });
-    })
-  );
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+    '/',
+    '/styles.css',
+    '/index.html'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
- 
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
- 
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
 });
