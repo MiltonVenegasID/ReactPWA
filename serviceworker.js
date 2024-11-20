5d8d7fcc0506ef7e704839644d85c87fe581e6b8
MiltonVenegas <miltonprofesional15@gmail.com>
2024-11-19 19:48:06 -0600

    God

--- serviceworker.js
+++ serviceworker.js
@@ -1,22 +1,20 @@
-var staticCacheName = "pwa2";
 
+var staticCacheName = "pwa5";
+ 
 self.addEventListener("install", function (e) {
-e.waitUntil(
-	caches.open(staticCacheName).then(function (cache) {
-	return cache.addAll(["/"])
-		.catch(function (error) {
-			console.error("Failed to cache resources:", error);
-		});
-	})
-);
+  e.waitUntil(
+    caches.open(staticCacheName).then(function (cache) {
+      return cache.addAll(["/"]);
+    })
+  );
 });
-
+ 
 self.addEventListener("fetch", function (event) {
-console.log(event.request.url);
-
-event.respondWith(
-	caches.match(event.request).then(function (response) {
-	return response || fetch(event.request);
-	})
-);
+  console.log(event.request.url);
+ 
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    })
+  );
 });
